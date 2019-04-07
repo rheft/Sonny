db41ac5f70af664bbd494a98d4cb122b66f7697f
Rob Heft <uheftr@gmail.com>
2019-04-07 06:18:03 -0400

    Those who bought ..., also bought ...

--- Association_Rule_Learning/Section 28 - Apriori/apriori.py
+++ Association_Rule_Learning/Section 28 - Apriori/apriori.py
@@ -1,19 +1,44 @@
 # Apriori
 
 # Importing the libraries
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
 import numpy as np
-import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+# Import lib files
+envs = load_dotenv(find_dotenv(usecwd=True))
+file = os.getenv("lib")
+sys.path.insert(0, file)
+print(file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Import the apriori classifier
+from apyori import apriori
 
 # Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+dataset = LoadData("Market_Basket_Optimisation.csv", header=None).data
+
+# Get transactions
 transactions = []
-for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+for i in range(0,dataset.shape[0]):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,dataset.shape[1])])
 
-# Training Apriori on the dataset
-from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+# create the model
+rules = apriori(
+    transactions,
+    min_support=0.003,
+    min_confidence=0.2,
+    min_lift=3,
+    min_length=2
+)
 
-# Visualising the results
+# Visualize!
 results = list(rules)
+results
--- lib/utils.py
+++ lib/utils.py
@@ -6,11 +6,16 @@ from dotenv import load_dotenv, find_dotenv
 
 
 class LoadData():
-    def __init__(self, dataset, sub_dir=""):
+    def __init__(self, dataset, sub_dir="", **kwargs):
         envs = load_dotenv(find_dotenv())
         self._file = os.getenv("data_file")
         full_path = self._file + sub_dir + "/" + dataset
-        self._data = pd.read_csv(full_path)
+        self._header = kwargs.get('header', False)
+        if self._header is not None:
+            self._data = pd.read_csv(full_path)
+        else:
+            self._data = pd.read_csv(full_path, header=self._header)
+
 
     @property
     def data(self):
