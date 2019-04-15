4e0d2b8b8aa58bad48754321a734efc5118348a3
Rob Heft <uheftr@gmail.com>
2019-04-15 06:06:38 -0400

    Deep learned the deep learning stuff

--- Deep_Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
+++ Deep_Learning/Section 39 - Artificial Neural Networks (ANN)/ann.py
@@ -1,77 +1,66 @@
-# Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
+# Imports
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
 import numpy as np
-import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-# Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
-
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
+# Import Deep Learning libraries (keras)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
+# Import lib files
+envs = load_dotenv(find_dotenv())
+file = os.getenv("lib")
+sys.path.insert(0, file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Import data
+dataset = LoadData("Churn_Modelling.csv").data
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Lets do some preprocessing...
+processor = PreProcessing()
+# Encode the data (Country/Gender)
+X[:, 1] = processor.encode(X[:, 1])
+X[:, 2] = processor.encode(X[:, 2])
+X = processor.hot_encoding(data = X, features=[1])
+X = X[:, 1:]
+
+# Split the data into training+test
+X_train, X_test, y_train, y_test = processor.split(X, y, test_size=0.2)
+
+# Apply feature scaling
+X_train = processor.fit_scaler(X_train)
+X_test = processor.fit_scaler(X_test)
+
+# Initialize the Artificial Neural Network (ANN)
 classifier = Sequential()
+# Create the input and first hidden layers
+classifier.add(Dense(input_dim=11, activation='relu', units=8, kernel_initializer='uniform'))
+# Create the second hidden layer
+classifier.add(Dense(activation='relu', units=8, kernel_initializer='uniform'))
+# Create the output layer
+classifier.add(Dense(activation='sigmoid', units=1, kernel_initializer='uniform'))
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+# Compile ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Adding the second hidden layer
-classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+# Lets Fit the ANN now...
+classifier.fit(X_train, y_train, batch_size=5, nb_epoch=250)
 
-# Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
-
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
-# Part 3 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
+# Predicting the test results
 y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
+y_pred_values = [1 if y > 0.5 else 0 for y in y_pred]
 
-# Making the Confusion Matrix
+# Making the confusion matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_test, y_pred_values)
+cm
--- Deep_Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/cnn.py
+++ Deep_Learning/Section 40 - Convolutional Neural Networks (CNN)/Convolutional_Neural_Networks/cnn.py
@@ -1,50 +1,51 @@
-# Convolutional Neural Network
+# Imports
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
+import numpy as np
+np.set_printoptions(suppress=True)
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
-
-# Importing the Keras libraries and packages
+# Import Deep Learning libraries (keras)
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-
-# Initialising the CNN
-classifier = Sequential()
-
-# Step 1 - Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
-
-# Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Adding a second convolutional layer
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Step 3 - Flattening
-classifier.add(Flatten())
-
-# Step 4 - Full connection
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
-
-# Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Part 2 - Fitting the CNN to the images
-
 from keras.preprocessing.image import ImageDataGenerator
 
+# Import lib files
+envs = load_dotenv(find_dotenv())
+file = os.getenv("lib")
+sys.path.insert(0, file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Building the CNN
+classifier = Sequential()
+
+# Step 1 -  Convolution
+classifier.add(Convolution2D(nb_filter=32, nb_row=3, nb_col=3, input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Step 3 - FLattening
+classifier.add(Flatten())
+
+# Step 4 - Full Connection
+classifier.add(Dense(activation='relu', units=128))
+classifier.add(Dense(activation='sigmoid', units=1))
+
+# Compile the net.
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fit the CNN to the images
+# This creates random slight changes to images in teh training set in order
+#   to create more images with more variance
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
@@ -52,18 +53,18 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('cnn_datasets/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('cnn_datasets/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
+                         steps_per_epoch = 250,
                          nb_epoch = 25,
                          validation_data = test_set,
                          nb_val_samples = 2000)
