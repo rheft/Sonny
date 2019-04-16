23fe6b3db63896e18abf738638c58aea637dd860
Rob Heft <uheftr@gmail.com>
2019-04-16 06:17:30 -0400

    Dimensionality Reduction

--- Dimensionality_Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
+++ Dimensionality_Reduction/Section 43 - Principal Component Analysis (PCA)/pca.py
@@ -1,76 +1,56 @@
-# PCA
-
-# Importing the libraries
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
 import numpy as np
-import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-# Importing the dataset
-dataset = pd.read_csv('Wine.csv')
+# Import lib files
+envs = load_dotenv(find_dotenv())
+file = os.getenv("lib")
+sys.path.insert(0, file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Import model library
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+
+# Import data
+dataset = LoadData("Wine.csv").data
+
+# Split the dataset
 X = dataset.iloc[:, 0:13].values
 y = dataset.iloc[:, 13].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# Lets do some preprocessing...
+processor = PreProcessing()
+# Split the data
+X_train, X_test, y_train, y_test = processor.split(X, y, test_size=0.2)
+# scale the data
+X_train = processor.fit_scaler(X_train)
+X_test = processor.scale(X_test)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Applying PCA
-from sklearn.decomposition import PCA
+# Apply PCA
 pca = PCA(n_components = 2)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 explained_variance = pca.explained_variance_ratio_
 
-# Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+# Lets fit the model now
+classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predict!
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+# Creating the confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
-plt.show()
+# Fine, lets visualize it.. I geuss its more fun 🤷‍
+visual = ClassifierVisual(X_train, y_train, classifier)
+visual.visualize(title='Logistic Regression', xlab='Age', ylab='Salary', colors=('red', 'green', 'blue'))
--- lib/visuals.py
+++ lib/visuals.py
@@ -8,16 +8,16 @@ class ClassifierVisual:
         self.y_set = y
         self.classifier = classifier
 
-    def visualize(self, title = 'Title', xlab = 'X Label', ylab = 'Y Label'):
+    def visualize(self, title = 'Title', xlab = 'X Label', ylab = 'Y Label', colors=('red', 'green')):
         X1, X2 = np.meshgrid(np.arange(start = self.X_set[:, 0].min() - 1, stop = self.X_set[:, 0].max() + 1, step = 0.01),
                              np.arange(start = self.X_set[:, 1].min() - 1, stop = self.X_set[:, 1].max() + 1, step = 0.01))
         plt.contourf(X1, X2, self.classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                     alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+                     alpha = 0.75, cmap = ListedColormap(colors))
         plt.xlim(X1.min(), X1.max())
         plt.ylim(X2.min(), X2.max())
         for i, j in enumerate(np.unique(self.y_set)):
             plt.scatter(self.X_set[self.y_set == j, 0], self.X_set[self.y_set == j, 1],
-                        c = ListedColormap(('red', 'green'))(i), label = j)
+                        c = ListedColormap(colors)(i), label = j)
         plt.title(title)
         plt.xlabel(xlab)
         plt.ylabel(ylab)
