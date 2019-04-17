b3deaa331080c8ed4e4f4f779544c3cd54658003
Rob Heft <uheftr@gmail.com>
2019-04-17 06:10:21 -0400

    Better methods for calculating accuracy

--- Model_Selection_&_Boosting/Section 48 - Model Selection/grid_search.py
+++ Model_Selection_&_Boosting/Section 48 - Model Selection/grid_search.py
@@ -1,88 +1,69 @@
-# Grid Search
-
-# Importing the libraries
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
 import numpy as np
-import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+# Import lib files
+envs = load_dotenv(find_dotenv())
+file = os.getenv("lib")
+sys.path.insert(0, file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Import model library
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+# Import data
+dataset = LoadData("Social_Network_Ads.csv").data
+
+# Split the dataset
+X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# Lets do some preprocessing...
+processor = PreProcessing()
+# Split the data
+X_train, X_test, y_train, y_test = processor.split(X, y, test_size=0.25)
+# scale the data
+X_train = processor.fit_scaler(X_train)
+X_test = processor.scale(X_test)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Fitting Kernel SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
+# Lets fit the model now
+classifier = SVC(kernel='rbf', random_state=0)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predict!
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+# Creating the confusion matrix
 cm = confusion_matrix(y_test, y_pred)
+cm
 
-# Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-accuracies.mean()
-accuracies.std()
+# Implementing k-fold cross validaiton
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+avg = accuracies.mean()
+std_dev_acc = accuracies.std()
+avg
+std_dev_acc
 
-# Applying Grid Search to find the best model and the best parameters
-from sklearn.model_selection import GridSearchCV
-parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-              {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
-grid_search = GridSearchCV(estimator = classifier,
-                           param_grid = parameters,
-                           scoring = 'accuracy',
-                           cv = 10,
-                           n_jobs = -1)
+# Applying grid search
+parameters = [
+    {"C": [1.0, 10, 100, 1000], "kernel": ["linear"]},
+    {"C": [1.0, 10, 100, 1000], "kernel": ["rbf"], "gamma":[0.3, 0.4, 0.5, 0.6, 0.7]}
+]
+grid_search = GridSearchCV(estimator=classifier, param_grid=parameters, scoring='accuracy', cv=10, n_jobs=-1)
 grid_search = grid_search.fit(X_train, y_train)
-best_accuracy = grid_search.best_score_
-best_parameters = grid_search.best_params_
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+grid_search.best_score_
+grid_search.best_params_
+# Fine, lets visualize it.. I geuss its more fun 🤷‍
+visual = ClassifierVisual(X_train, y_train, classifier)
+visual.visualize(title='Linear SVM', xlab='Age', ylab='Salary')
--- Model_Selection_&_Boosting/Section 48 - Model Selection/k_fold_cross_validation.py
+++ Model_Selection_&_Boosting/Section 48 - Model Selection/k_fold_cross_validation.py
@@ -1,75 +1,57 @@
-# k-Fold Cross Validation
-
-# Importing the libraries
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
 import numpy as np
-import matplotlib.pyplot as plt
+np.set_printoptions(suppress=True)
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
+# Import lib files
+envs = load_dotenv(find_dotenv())
+file = os.getenv("lib")
+sys.path.insert(0, file)
+from utils import LoadData
+from preprocessing import PreProcessing
+from visuals import ClassifierVisual
+
+# Import model library
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+
+# Import data
+dataset = LoadData("Social_Network_Ads.csv").data
+
+# Split the dataset
+X = dataset.iloc[:, [2,3]].values
 y = dataset.iloc[:, 4].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# Lets do some preprocessing...
+processor = PreProcessing()
+# Split the data
+X_train, X_test, y_train, y_test = processor.split(X, y, test_size=0.25)
+# scale the data
+X_train = processor.fit_scaler(X_train)
+X_test = processor.scale(X_test)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Fitting Kernel SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
+# Lets fit the model now
+classifier = SVC(kernel='rbf', random_state=0)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predict!
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+# Creating the confusion matrix
 cm = confusion_matrix(y_test, y_pred)
+cm
 
-# Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-accuracies.mean()
-accuracies.std()
-
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+# Implementing k-fold cross validaiton
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+avg = accuracies.mean()
+avg
+std_dev_acc = accuracies.std()
+std_dev_acc
+# Fine, lets visualize it.. I geuss its more fun 🤷‍
+visual = ClassifierVisual(X_train, y_train, classifier)
+visual.visualize(title='Linear SVM', xlab='Age', ylab='Salary')
